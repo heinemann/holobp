c17f8071c3704fa2fa4e9ba89cc7eba82e906938
allgru <contact@allangrunert.de>
2017-08-12 12:02:43 +0200

    Verbesserung

--- src/main/java/de/holobp/modul/benutzer/User.java
+++ src/main/java/de/holobp/modul/benutzer/User.java
@@ -1,59 +1,76 @@
+/**
+ * Atomare Kalsse für Benutzer 
+ * 
+ * @author Adrian Heinemann <post@heinemann.de>
+ * @author Allan Grunert <contact@allangrunert.de>
+ */
 package de.holobp.modul.benutzer;
 
 import java.sql.Timestamp;
 
 public class User {
+
 	private String username;
 	private String password;
 	private String email;
-	
-	private Timestamp date_creation;
-	
-	private Timestamp date_modified;
-	
-	public Timestamp getDate_creation() {
-		return date_creation;
+	private String uid;
+	private Timestamp dateCreation;
+	private Timestamp dateModified;
+
+	public Timestamp getDateCreation() {
+		return dateCreation;
 	}
-	public void setDate_creation(Timestamp date_creation) {
-		this.date_creation = date_creation;
+
+	public void setDateCreation(Timestamp dateCreation) {
+		this.dateCreation = dateCreation;
 	}
-	public Timestamp getDate_modified() {
-		return date_modified;
+
+	public Timestamp getDateModified() {
+		return dateModified;
 	}
-	public void setDate_modified(Timestamp date_modified) {
-		this.date_modified = date_modified;
+
+	public void setDate_modified(Timestamp dateModified) {
+		this.dateModified = dateModified;
 	}
+
 	private int id;
-	
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getUsername() {
 		return username;
 	}
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
-	
+
 	@Override
 	public String toString() {
-		return "User [username=" + username + ", password=" + password + ", email=" + email + ",date_creation=" + date_creation + ",date_modified=" + date_modified + ", id=" + id + "]";
+		return "User [username=" + username + ", password=" + password + ", email=" + email + ",dateCreation="
+				+ dateCreation + ",dateModified=" + dateModified + ", id=" + id + ", uid=" + uid + "]";
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
+
 }
